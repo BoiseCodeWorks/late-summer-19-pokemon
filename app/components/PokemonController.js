ff16a25f9f8dd7b33f08061eebe373a9fc27d649
Darryl Kilzer <darrylkilzer@gmail.com>
2019-06-25 11:53:10 -0600

    finished

--- app/components/PokemonController.js
+++ app/components/PokemonController.js
@@ -8,15 +8,49 @@ function drawPokemon() {
     let pokemon = _pokemonService.Pokemon
     let template = ''
     pokemon.forEach(p => template += `
-        <button class="col-2 btn btn-outline-info m-3">${p.name}</button>
+        <button class="col-2 btn btn-outline-info m-3" onclick="app.controllers.pokemonController.getSelectedPokemon('${p.name}')">${p.name}</button>
         `
     )
     pokemonElem.innerHTML = template
 }
 
+function drawMyPokemon(){
+    let myPokemonElem = document.querySelector("#my-pokemon")
+    let myPokemon = _pokemonService.MyPokemon
+    let template = ''
+    myPokemon.forEach(p =>{
+        template += p.Template
+    })
+    myPokemonElem.innerHTML = template
+}
+
+function drawSelectedPokemon(){
+    let selectedElem = document.querySelector("#selected-pokemon")
+    let selectedPokemon = _pokemonService.SelectedPokemon
+    selectedElem.innerHTML = selectedPokemon.Template
+    // console.log('draw selected',selectedPokemon)
+}
+
+
+
 export default class PokemonController {
     constructor() {
         _pokemonService.addSubscriber("pokemon", drawPokemon)
+        _pokemonService.addSubscriber('selectedPokemon', drawSelectedPokemon)
+        _pokemonService.addSubscriber('myPokemon', drawMyPokemon)
         _pokemonService.getPokemon()
+        _pokemonService.getMyPokemon()
+    }
+
+    getSelectedPokemon(name){
+        _pokemonService.getSelectedPokemon(name)
+    }
+
+    savePokemon(){
+        _pokemonService.savePokemon()
+    }
+
+    deletePokemon(id){
+        _pokemonService.deletePokemon(id)
     }
 }
--- app/components/PokemonService.js
+++ app/components/PokemonService.js
@@ -1,15 +1,23 @@
 import Pokemon from "../models/Pokemon.js";
 
 let pokeAPI = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/pokemon'
+    baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+})
+
+let bcwApi = axios.create({
+    baseURL: 'https://bcw-sandbox.herokuapp.com/api/Darryl/pokemon'
 })
 
 let _state = {
-    pokemon: []
+    pokemon: [],
+    myPokemon: [],
+    selectedPokemon: {}
 }
 
 let _subscribers = {
-    pokemon: []
+    pokemon: [],
+    myPokemon:[],
+    selectedPokemon: []
 }
 
 function setState(propName, data) {
@@ -18,19 +26,65 @@ function setState(propName, data) {
 }
 
 export default class PokemonService {
-
+    
     get Pokemon() {
         return _state.pokemon.map(p => p)
+    }
+
+    get SelectedPokemon(){
+        return new Pokemon(_state.selectedPokemon)
+    }
+
+    get MyPokemon(){
+        return _state.myPokemon.map(p => new Pokemon(p))
     }
 
     addSubscriber(propName, fn) {
         _subscribers[propName].push(fn)
     }
 
+    deletePokemon(id) {
+        bcwApi.delete(id)
+        .then(res=>{
+            console.log(res.data.message)
+            this.getMyPokemon()
+        })
+        .catch(err => console.error(err))
+    }
+
+    savePokemon() {
+        bcwApi.post('', this.SelectedPokemon)
+            .then(res=>{
+                console.log(res.data.message)
+                this.getMyPokemon()
+            })
+            .catch(err => console.error(err))
+    }
+
+    getSelectedPokemon(name) {
+        pokeAPI.get(name)
+            .then(res=>{
+                console.log("selected request",res)
+                setState('selectedPokemon', res.data)
+            })
+            .catch(err=> console.error(err))
+
+    }
+
+    getMyPokemon(){
+        bcwApi.get()
+            .then(res=>{
+                console.log("gets my pokemon", res.data.data)
+                setState('myPokemon', res.data.data)
+            })
+            .catch(err=> console.error(err))
+    }
+
+
     getPokemon() {
         pokeAPI.get()
             .then(res => {
-                console.log(res.data)
+                console.log("all pokemon request",res.data)
                 setState("pokemon", res.data.results)
             })
             .catch(err => console.error(err))
